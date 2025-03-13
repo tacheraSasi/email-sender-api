2667b4c4f716022a336df30cb0d2cda012529a02
Tachera Sasi <tacherasasi@gmail.com>
2025-03-13 16:55:10 +0300

    config module forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmailModule],
+  imports: [
+    ConfigModule.forRoot(),
+    EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
