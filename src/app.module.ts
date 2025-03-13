ee85eaf01bb480e5659353806e95914e77683f03
Tachera Sasi <tacherasasi@gmail.com>
2025-03-13 16:58:59 +0300

    type orm configureations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    EmailModule],
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || '',
+      database: process.env.DB_NAME || 'emaildb',
+      entities: [Email],
+      synchronize: true, // Auto creates tables (Not recommended in production)
+    }),
+    ,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
